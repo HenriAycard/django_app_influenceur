fc05358b15da8264dec808be4d8f21e3ca0d77c6
HenriAycard <henriaycard@hotmail.fr>
2024-06-02 15:44:47 +0000

    update project

--- Frontend/In-Touch/src/app/app.module.ts
+++ Frontend/In-Touch/src/app/app.module.ts
@@ -4,25 +4,20 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy, Platform, LoadingController } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TokenInterceptor } from './services/token.interceptor';
 
 
-@NgModule({
-  declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(),
-    //NativeHttpModule,
-    TranslateModule.forRoot(),
-    AppRoutingModule,
-    HttpClientModule],
-  providers: [
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    //{provide: HttpBackend, useClass: NativeHttpFallback, deps: [Platform, NativeHttpBackend, HttpXhrBackend]},
-    { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true } 
-  ],
-  bootstrap: [AppComponent],
-})
+@NgModule({ declarations: [AppComponent],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        IonicModule.forRoot(),
+        //NativeHttpModule,
+        TranslateModule.forRoot(),
+        AppRoutingModule], providers: [
+        { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+        //{provide: HttpBackend, useClass: NativeHttpFallback, deps: [Platform, NativeHttpBackend, HttpXhrBackend]},
+        { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true },
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class AppModule {}
